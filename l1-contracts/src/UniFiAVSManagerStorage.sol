892178ec43d8a71ae1b5c0536dc5c61d88452367
Benjamin <benjaminxh+github@gmail.com>
2025-04-11 10:54:35 +0200

    restore old storage layout

--- l1-contracts/src/UniFiAVSManagerStorage.sol
+++ l1-contracts/src/UniFiAVSManagerStorage.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { IUniFiAVSManager } from "./interfaces/IUniFiAVSManager.sol";
+import { DeprecatedOperatorData } from "./structs/DeprecatedOperatorData.sol";
+
 /**
  * @title UniFiAVSManagerStorage
  * @author Puffer Finance
@@ -10,11 +12,16 @@ import { IUniFiAVSManager } from "./interfaces/IUniFiAVSManager.sol";
  */
 abstract contract UniFiAVSManagerStorage is IUniFiAVSManager {
     struct UniFiAVSStorage {
+        mapping(bytes32 => ValidatorData) _deprecated_validators;
+        mapping(uint256 => bytes32) _deprecated_validatorIndexes;
+        mapping(address => DeprecatedOperatorData) _deprecated_operators;
+        uint64 deregistrationDelay;
+        mapping(uint8 => uint256) _deprecated_bitmapIndexToChainId;
+        mapping(uint256 => uint8) _deprecated_chainIdToBitmapIndex;
+        EnumerableSet.AddressSet allowlistedRestakingStrategies;
         mapping(bytes validatorPubkey => ValidatorData validatorData) validators;
         mapping(uint256 validatorIndex => bytes validatorPubkey) validatorIndexes;
         mapping(address operator => OperatorData operatorData) operators;
-        uint64 deregistrationDelay;
-        EnumerableSet.AddressSet allowlistedRestakingStrategies;
     }
 
     /**
